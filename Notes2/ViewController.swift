cbddff4cf3744003d267e23a3ea3e8d56609080a
Srivinayak Chaitanya Eshwa <eshwavin@hotmail.com>
2020-05-25 04:37:59 +0200

    Initial Commit

---
+++ Notes2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes2
+//
+//  Created by Srivinayak Chaitanya Eshwa on 25/05/20.
+//  Copyright © 2020 Srivinayak Chaitanya Eshwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
