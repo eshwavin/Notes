55f0e5b5c316194149bd9f136fe1be04214499b8
Srivinayak Chaitanya Eshwa <eshwavin@hotmail.com>
2020-05-25 04:40:42 +0200

    Added master detail controller

---
+++ Notes2/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Notes2
+//
+//  Created by Srivinayak Chaitanya Eshwa on 25/05/20.
+//  Copyright © 2020 Srivinayak Chaitanya Eshwa. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
